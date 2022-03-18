dfc2814dbffc42575c3a86b30d5feab7ed55d9ba
Nguyễn Văn Quảng <62205080+ntvquang@users.noreply.github.com>
2022-03-18 16:00:16 +0700

    delete count document

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -30,12 +30,11 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
-        const course = new Course(formData);
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(req.body);
         course
             .save()
-            .then(() => res.redirect("/"))
+            .then(() => res.redirect("/me/stored/courses"))
             .catch((error) => {});
     }
 
@@ -48,9 +47,36 @@ class CourseController {
 
     // [DELETE] /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
             .then(() => res.redirect("back"))
             .catch(next);
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
+    }
+
+    // [POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case "delete":
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect("back"))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: "Action is invalid" });
+        }
     }
 }
 
